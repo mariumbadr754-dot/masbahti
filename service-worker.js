e67d0b28cf17f17ff28729df18b0d6b07abbab24
mariumbadr754-dot <mariumbadr754@gmail.com>
2026-08-26 02:57:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,79 +1,61 @@
-const CACHE_NAME = "masbahati-v1";
+const CACHE_NAME = "masbahati-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./logo.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 
   self.skipWaiting();
 });
 
-
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheName !== CACHE_NAME) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 
   self.clients.claim();
 });
 
-
 self.addEventListener("fetch", event => {
-
   if (event.request.method !== "GET") {
     return;
   }
 
   event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
+    fetch(event.request)
+      .then(networkResponse => {
+        const responseClone = networkResponse.clone();
 
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return fetch(event.request)
-          .then(networkResponse => {
-
-            const clonedResponse =
-              networkResponse.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(
-                  event.request,
-                  clonedResponse
-                );
-              });
-
-            return networkResponse;
-
-          })
-          .catch(() => {
-            return caches.match("./index.html");
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseClone);
           });
 
+        return networkResponse;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(cachedResponse => {
+            return cachedResponse || caches.match("./index.html");
+          });
       })
   );
-
 });
